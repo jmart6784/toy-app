d4b6edaab67f517dd8957d1c70081be8d8e468b8
Juan <jmartinez6784@yahoo.com>
2020-04-06 22:44:25 -0700

    Finished toy app

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :microposts
+  validates :name, length: {maximum: 12},
+  presence: true
+  validates :email, length: {maximum: 35},
+  presence: true
+end
